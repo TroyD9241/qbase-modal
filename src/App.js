bc733cf516a13735b9b1e0c6173b611b26a9d6d2
Troy <troyd9216@gmail.com>
2022-01-28 20:37:12 -0600

    added eslint config, created a navbar tailwind component will be moving further component development to a different branch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import { useState } from "react";
-import { ComponentModal } from "./ComponentModal/ComponentModal";
+import { Navbar } from "./Navbar/Navbar";
 
 function App() {
   const [modal, setModal] = useState(false);
   const toggle = () => setModal(!modal);
   return (
-    <div className="bg-slate-500">
-      <div className="app-btn">
-        <button
-          variant="outline-warning"
-          size="lg"
-          onClick={() => toggle()}
-          className="btn"
-        >
-          Buy & Sell
-        </button>
-      </div>
-      <></>
-      <ComponentModal show={modal} />
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
 
