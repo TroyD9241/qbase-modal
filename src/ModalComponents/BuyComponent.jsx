2b151de948f96c4725233f8d614afaec053ed1a1
Troy <troyd9216@gmail.com>
2022-01-28 22:16:15 -0600

    started creating buycomponent

--- src/ModalComponents/BuyComponent.jsx
+++ src/ModalComponents/BuyComponent.jsx
@@ -1,10 +1,69 @@
+import { useState } from "react";
+
+const accounts = [
+  {
+    name: "troy",
+  },
+
+  {
+    name: "demo",
+  },
+];
+
+const assetList = [
+  {
+    name: "Stock",
+  },
+  {
+    name: "Crypto",
+  },
+];
+
+const stockInfo = [
+  {
+    ticker: "AAPL",
+    price: "200.22",
+    trend: "+2.3%",
+  },
+  {
+    ticker: "GGL",
+    price: "300",
+    trend: "+3.4%",
+  },
+];
+
 export const BuyComponent = () => {
-    return (
-        <>
-            <h1>Hiii</h1>
-            <div>
-                gwpojepwojpojegws
-            </div>
-        </>
-    )
-}
+  const [inputAmount, setInputAmount] = useState(0);
+  const [openMenu, setOpenMenu] = useState(false);
+  const [show, setShow] = useState(false);
+
+  const toggle = () => {
+    setOpenMenu(!openMenu);
+  };
+  return (
+    <div id="account-buttons">
+      <div id="asset-selector" className="relative inline-block">
+        <button
+          class="relative z-10 block p-2 text-gray-700 bg-white border border-transparent rounded-md
+            dark:text-white focus:border-blue-500 focus:ring-opacity-40 dark:focus:ring-opacity-40 focus:ring-blue-300
+            dark:focus:ring-blue-400 focus:ring dark:bg-gray-800 focus:outline-none"
+          onClick={() => toggle()}
+        >
+          {accounts[0].name}
+        </button>
+
+        {openMenu ? (
+          <div class="absolute right-0 z-20 w-48 py-2 mt-2 bg-white rounded-md shadow-xl dark:bg-gray-800">
+            <a
+              href="#"
+              class="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              {" "}
+              your profile{" "}
+            </a>
+          </div>
+        ) : null}
+      </div>
+    </div>
+  );
+};
--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,20 +1,22 @@
-import {useState} from 'react'
-import {ComponentModal} from '../ComponentModal/ComponentModal'
+import { useState } from "react";
+import { ComponentModal } from "../ComponentModal/ComponentModal";
 
 export const Navbar = () => {
-    const [modal, setModal] = useState(false);
-    const toggle = () => setModal(!modal);
-    return (
-    <nav class="bg-white shadow dark:bg-slate-100">
-    <div class="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize dark:text-gray-300">
-
-                <button class="px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-orange-300 rounded-md hover:bg-orange-500 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-80"
-                onClick={() => toggle()}
-                >
-        Buy & Sell
-                </button>
-                <ComponentModal show={modal} />
-    </div>
-</nav>
-    )
-}
+  const [modal, setModal] = useState(false);
+  const toggle = () => setModal(!modal);
+  return (
+    <>
+      <nav class="bg-white shadow dark:bg-slate-100">
+        <div class="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize dark:text-gray-300">
+          <button
+            class="px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-orange-300 rounded-md hover:bg-orange-500 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-80"
+            onClick={() => toggle()}
+          >
+            Buy & Sell
+          </button>
+        </div>
+      </nav>
+      <ComponentModal show={modal} />
+    </>
+  );
+};
